8768c55825a18899a76359a0d75fd1ae17461814
booger <bonappetite2580@gmail.com>
2026-05-30 13:00:55 +0800

    5/30 hw10 sql

---
+++ cs61a/hw/hw10/hw10.sql
@@ -0,0 +1,72 @@
+CREATE TABLE parents (parent TEXT, child TEXT);
+
+INSERT INTO parents VALUES
+  ('ace', 'bella'),
+  ('ace', 'charlie'),
+  ('daisy', 'hank'),
+  ('finn', 'ace'),
+  ('finn', 'daisy'),
+  ('finn', 'ginger'),
+  ('ellie', 'finn');
+
+CREATE TABLE dogs (name TEXT, fur TEXT, height INTEGER);
+
+INSERT INTO dogs VALUES
+  ('ace',     'long',  26),
+  ('bella',   'short', 52),
+  ('charlie', 'long',  47),
+  ('daisy',   'long',  46),
+  ('ellie',   'short', 35),
+  ('finn',    'curly', 32),
+  ('ginger',  'short', 28),
+  ('hank',    'curly', 31);
+
+CREATE TABLE sizes (size TEXT, min INTEGER, max INTEGER);
+
+INSERT INTO sizes VALUES
+  ('toy',      24, 28),
+  ('mini',     28, 35),
+  ('medium',   35, 45),
+  ('standard', 45, 60);
+
+
+-- All dogs with parents ordered by decreasing height of their parent
+CREATE TABLE by_parent_height AS
+  SELECT child
+  FROM parents, dogs
+  WHERE parent = name 
+  ORDER BY height DESC;
+
+
+-- The size of each dog
+CREATE TABLE size_of_dogs AS
+  SELECT a.name, b.size 
+  FROM dogs a, sizes b
+  WHERE b.min < a.height AND a.height <= b.max;
+
+
+
+-- [Optional] Filling out this helper table is recommended
+CREATE TABLE siblings AS
+  SELECT a.child AS first_child, b.child AS second_child, c.size AS size
+  FROM parents a, parents b, size_of_dogs c, size_of_dogs d
+  WHERE a.parent = b.parent 
+  AND a.child < b.child 
+  AND c.name = a.child
+  AND b.child = d.name
+  AND c.size = d.size;
+
+-- Sentences about siblings that are the same size
+CREATE TABLE sentences AS
+  SELECT 'The two siblings, ' || first_child || ' and ' || second_child || ', have the same size: ' || size
+  FROM siblings;
+
+
+-- Height range for each fur type where all of the heights differ by no more than 30% from the average height
+CREATE TABLE low_variance AS
+  SELECT fur, 
+  MAX(height) - MIN(height) AS range
+  FROM dogs 
+  GROUP BY fur
+  HAVING MIN(height) >= AVG(height) * 0.7
+  AND MAX(height) <= AVG(height) * 1.3;
